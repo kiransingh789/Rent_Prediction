975a165010bfb2e34b1e4adff2795fc7956b965f
kiransingh789 <kittu.789@gmail.com>
2023-03-05 23:32:59 -0600

    Add files via upload

---
+++ explore_page.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import pandas as pd
+
+## formulas from nb
+def clean_flooron(x):
+    if 'Gr' in str(x[:2]):
+        return 0
+    if 'Up' in str(x[:2]):
+        return -1
+    if 'Lo' in str(x[:2]):
+        return -2
+    else:
+        return str(x[:2])
+
+def clean_floortot(x):
+    if 'nd' in str(x[-2:]):
+        return 0
+    else:
+        return str(x[-2:])
+
+### caching preprocessing 
+@st.cache
+def load_data():
+    rent = pd.read_csv(r'House_Rent_Dataset.csv')
+
+    rent['floor on'] = rent['Floor'].apply(clean_flooron)
+    rent['floor total'] = rent['Floor'].apply(clean_floortot)
+
+    rent['Posted On'] = pd.to_datetime(rent['Posted On'])
+    rent['month'] = rent['Posted On'].dt.month
+    rent['year'] = rent['Posted On'].dt.year
+    rent['DOW'] = rent['Posted On'].dt.dayofweek
+
+    rent[['floor on', 'floor total']] = rent[['floor on', 'floor total']].astype(int)
+
+    rent = rent[['Bathroom','Size','City','Furnishing Status','Rent','Posted On','Point of Contact','BHK']]
+
+    return rent
+
+df = load_data()
+
+### Showing consumers valuable charts
+def show_explore_page():
+    st.write("## Explore Rent Prices in India")
+
+    st.write(
+        """
+    #### MagicBricks India Household Rent Dataset (2022)
+    """
+    )
+    data = df.groupby(['City'])['Rent'].mean().round(2).sort_values(ascending = True)
+    data2 = df.groupby(['Point of Contact'])['Rent'].mean().round(2).sort_values(ascending = True)
+    data3 = df.groupby(['Posted On'])['Rent'].mean().round(2).sort_values(ascending = True)
+    data4 = df.groupby(['Bathroom'])['Rent'].mean().round(2).sort_values(ascending = True)
+    data5 = df.groupby(['BHK'])['Rent'].mean().round(2).sort_values(ascending = True)
+
+    st.write("""#### Average Cost of Rent by City""")
+    st.bar_chart(data)
+
+    st.write("""#### Average Cost of Rent by Point of Contact""")
+    st.bar_chart(data2)
+
+    st.write("""#### Average Cost of Rent by Bathroom Count""")
+    st.bar_chart(data4)
+
+    st.write("""#### Average Cost of Rent by Bedroom + Kitchen + Hall Count""")
+    st.bar_chart(data5)
+
+    st.write("""#### Listings By Date in 2022""")
+    st.line_chart(data3)
---
+++ prediction_page.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import sklearn
+from sklearn.ensemble import RandomForestRegressor
+import pickle
+import numpy as np
+
+## Showing prediction page function with title / subtitle
+
+
+    ### loading pickle final model
+def load_model():
+    with open('model_labelencode.pkl', 'rb') as file:
+        data = pickle.load(file)
+    return data
+
+data = load_model()
+
+    ### Assign model and label encoders
+regressor = data['model']
+le_city = data['le_city']
+le_contact = data['le_contact']
+
+def show_prediction_page():
+    st.write("""## India Apartment & Home Rent Prediction""")
+
+    st.write("""#### Calculate My Rent: """)
+
+    ### Writing out categories for selector
+    cities = (
+        'Mumbai',
+        'Chennai',
+        'Bangalore',
+        'Hyderabad',
+        'Delhi',     
+        'Kolkata',
+        )
+
+
+    contact = (
+        'Contact Owner',
+        'Contact Agent',
+        'Contact Builder',
+    )
+
+
+    ### Creating selectors and sliders
+    city = st.selectbox('Select Your Preferred City', cities)
+    contacts = st.selectbox('Select How to Contact Landlord', contact)
+
+    size = st.slider('Select Preferred Size in Square Feet', min_value = 10, max_value = 10000)
+    bhk = st.slider('Select Preferred Bedroom / Hall / Kitchen Amount', min_value = 1, max_value = 30)
+    bathroom = st.slider('Select Preferred Bathroom Amount', min_value = 1, max_value = 10)
+    floor = st.slider('Select Prefered Building Floor Total', min_value = 0, max_value = 150)
+   
+    ### Creatng calculate button with model
+    ok = st.button('Calculate Rent')
+    if ok:
+        X = np.array([[size, city, bathroom, floor, contacts, bhk]])
+        X[:, 1] = le_city.transform(X[:, 1])
+        X[:, 4] = le_contact.transform(X[:, 4])
+        X = X.astype(float)
+
+        rent = regressor.predict(X)
+        st.write(f"#### The estimated rent is ₹{rent[0]:.2f}")
+        st.write(f"#### The estimated rent in USD is ${rent[0]*0.01214:.2f}")
